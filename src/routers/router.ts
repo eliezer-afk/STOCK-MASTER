5e844a6ad93e5aeecde53e1014e82b8fc0a53003
Eliezer <eliriper123@gmail.com>
2024-10-30 21:05:04 -0300

    Modifiqué

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { body, param } from 'express-validator'
-import { createProduct, deleteProduct, getProduct, getProductById, updateAvailability, updateProduct} from './handlers/product'
-import { handleInputErrors } from './middleware'
+import { createProduct, deleteProduct, getProduct, getProductById, updateAvailability, updateProduct} from '../handlers/product'
+import { handleInputErrors } from '../middleware'
 
 const router = Router()
 
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@
 
 import express from 'express'
 import colors from 'colors'
-import router from './router'
+import router from './routers/router'
 import db from './config/db'
 
 // Conectar a la BD
@@ -10,9 +10,9 @@ export async function connectDB() {
     try {
         await db.authenticate()
         db.sync()
-        //console.log(colors.bgBlue.white('Conexion Exitosa a la BD'))
+        console.log(colors.bgBlue.white('Conexion Exitosa a la BD'))
     } catch (error) {
-        // console.log(error)
+        console.log(error)
         console.log(colors.bgRed.white('Hubo un error al conectar a la BD'))
     }
 }
