09df913d562470fe98be2db28e042407fbf7d341
Eliezer <eliriper123@gmail.com>
2025-02-12 12:13:49 -0300

    funciona en docker !!!!!!!!!!!!!!!

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-// Archivo principal
-
 import server from "./server";
 import colors from 'colors'
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3000
 
 server.listen(port, () => {
     console.log(colors.bgCyan(`REST API en el puesto ${port}`))
